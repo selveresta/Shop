8a376d189edaf39984300b3f2b7e1b8159dbc172
artem <artemonlypnyk@ukr.net>
2022-08-11 15:26:03 +0300

    Add queries on main function app, need decomponize

--- src/App.js
+++ src/App.js
@@ -6,39 +6,83 @@ import ClotherList from "./Components/ClotherList";
 import "./styles/App.css";
 import Counter from "./Components/Counter";
 import CartClotherItem from "./Components/CartClotherItem";
-import { GET_ALL_PRODUCT, GET_CATEGORY_DATA, GET_CURRENCIES, GET_ID_PRODUCTS } from "./Data/Query";
+import {
+	GET_ALL_PRODUCT,
+	GET_CATEGORIES_NAMES,
+	GET_CATEGORY_DATA,
+	GET_CURRENCIES,
+	GET_ID_PRODUCTS,
+} from "./Data/Query";
 import { useQuery } from "@apollo/client";
+import CategoryName from "./Components/CategoryName";
 
 function App() {
-	const [clothers, setClothers] = useState([
-		{ pic: Clother, type: "", size: {}, color: {}, name: "name1", price: "100", link: "#" },
-		{ pic: Clother, type: "", size: {}, color: {}, name: "name2", price: "100", link: "#" },
-		{ pic: Clother, type: "", size: {}, color: {}, name: "name3", price: "100", link: "#" },
-		{ pic: Clother, type: "", size: {}, color: {}, name: "name4", price: "100", link: "#" },
-		{ pic: Clother, type: "", size: {}, color: {}, name: "name5", price: "100", link: "#" },
-		{ pic: Clother, type: "", size: {}, color: {}, name: "name6", price: "100", link: "#" },
-	]);
+	//---------------------------------------------------------
+	const getCategories = useQuery(GET_CATEGORIES_NAMES);
+	const [categories, setCategories] = useState([]);
 
-	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		if (!getCategories.loading) {
+			setCategories(getCategories.data.categories);
+		}
+	}, [getCategories.data]);
 
-	// const { data, loading, error } = useQuery(GET_ALL_PRODUCT);
-	// const { data, loading, error } = useQuery(GET_ID_PRODUCTS);
+	const [currentCategory, setCurrentCategory] = useState("ALL");
 
-	// useEffect(() => {
-	// 	if (!loading) {
-	// 		setProducts(data.categories[0].products);
-	// 	}
-	// }, [data]);
+	const newCategory = (category) => {
+		setCurrentCategory(category);
+	};
 
-	// console.log(products);
-	// console.log(currencies);
+	//---------------------------------------------------------
+
+	const getCurrencies = useQuery(GET_CURRENCIES);
+	const [currencies, setCurrencies] = useState([]);
+
+	useEffect(() => {
+		if (!getCurrencies.loading) {
+			setCurrencies(getCurrencies.data.currencies);
+		}
+	}, [getCurrencies.data]);
+
+	const [currentCurrency, setCurrentCurrency] = useState("$");
+
+	const newCurrentCurrency = (symbol) => {
+		setCurrentCurrency(symbol);
+	};
+	//---------------------------------------------------------
+
+	const getProducts = useQuery(GET_CATEGORY_DATA);
+	const [allArraysProducts, setAllArraysProducts] = useState([]);
+
+	useEffect(() => {
+		if (!getProducts.loading) {
+			setAllArraysProducts(getProducts.data.categories);
+		}
+	}, [getProducts.data]);
+
+	const [currentCategoryProducts, setCurrentCategoryProducts] = useState([]);
+
+	useEffect(() => {
+		allArraysProducts.forEach((element) => {
+			if (element.name.toLowerCase() == currentCategory.toLowerCase()) {
+				setCurrentCategoryProducts(element.products);
+			}
+		});
+	}, [currentCategory, allArraysProducts]);
+
+	// const getFullProducts = useQuery(GET_ALL_PRODUCT);
 
 	return (
 		<div className='App'>
-			<Header></Header>
-			<h1 className='categoryName'>Category Name</h1>
-			{/* <ClotherList clothers={clothers}></ClotherList> */}
-			{/* <ClotherList clothers={products}></ClotherList> */}
+			<Header
+				categories={categories}
+				currencies={currencies}
+				currentCurrency={currentCurrency}
+				newCurrentCurrency={newCurrentCurrency}
+				newCategory={newCategory}></Header>
+			<CategoryName currentCategory={currentCategory}></CategoryName>
+			<ClotherList products={currentCategoryProducts} currentCurrency={currentCurrency}></ClotherList>
+			{/* <CartClotherItem></CartClotherItem> */}
 		</div>
 	);
 }
